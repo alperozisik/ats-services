11695a06a7f19ef23441b0d49dae1391b1b4fdd4
Alper Özışık <alper.ozisik@smartface.io>
2017-10-19 11:54:02 +0000

    added categories

---
+++ endpoints/categories.js
@@ -0,0 +1,21 @@
+const genericBodyHandler = require("../lib/generic-body-handler");
+const valueConverter = require("../lib/value-converter");
+
+module.exports = function(service, connectorName, validate) {
+    service.get('/mobile/custom/ats/appointment/category', validate, function(req, res) {
+        var oracleMobile = req.oracleMobile;
+        var lang = req.get("language") === "ar" ? 2 : 1;
+        oracleMobile.connectors.get(connectorName, `serviceWS/getAppointmentsCategories/${lang}`)
+            .then(
+                function(result) {
+                    var body = genericBodyHandler(result.result);
+                    body.forEach((item) => { valueConverter(item) });
+                    res.status(200).json(body);
+                },
+                function(error) {
+                    var body = genericBodyHandler(error.error);
+                    res.status(error.statusCode).json(body);
+                }
+            );
+    });
+};
