104e41ab17e9e41755fef30eb5108a0d3eea4da1
Alper Özışık <alper.ozisik@smartface.io>
2017-10-19 11:28:14 +0000

    added doctors & clinics

--- endpoints/doctors.js
+++ endpoints/doctors.js
@@ -1,16 +1,17 @@
 const genericBodyHandler = require("../lib/generic-body-handler");
 const companyId = 1;
+const valueConverter = require("../lib/value-converter");
+
 module.exports = function(service, connectorName, validate) {
-
-
-    service.get('/mobile/custom/ats/patient/:patientId', validate, function(req, res) {
+    service.get('/mobile/custom/ats/clinic/:clinicNo/doctors', validate, function(req, res) {
         var oracleMobile = req.oracleMobile;
-        var patientId = req.params.patientId;
         var lang = req.get("language") === "ar" ? 2 : 1;
-        oracleMobile.connectors.get(connectorName, `serviceWS/getPatientInfo/${companyId}/${patientId}/${lang}`)
+        var clinicNo = req.params.clinicNo;
+        oracleMobile.connectors.get(connectorName, `serviceWS/getDoctorOfDepartments/${companyId}/${clinicNo}/${lang}`)
             .then(
                 function(result) {
                     var body = genericBodyHandler(result.result);
+                    body.forEach((item) => { valueConverter(item) });
                     res.status(200).json(body);
                 },
                 function(error) {
